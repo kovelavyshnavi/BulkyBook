a2877d8f8b9ae57c5652b88cca1dc4d07dcab806
kovel <kovel@DESKTOP-3GSCTEV>
2021-08-05 18:27:35 +0530

    Section 7: Category Delete API Call and Category Delete Js

--- Areas/Admin/Controllers/CategoryController.cs
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -69,7 +69,17 @@ namespace BulkyBook.Areas.Admin.Controllers
         }
 
         [HttpDelete]
-        public IActionResult 
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Category.Get(id);
+            if(objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.Category.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successful" });
+        }
         #endregion
     }
 }
