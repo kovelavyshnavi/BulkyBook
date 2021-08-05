a3d297d5dcf1a8b0d35b256ccaad9e3e6adb64e3
kovel <kovel@DESKTOP-3GSCTEV>
2021-08-05 18:20:24 +0530

    Section 7: Category Upsert Post Action

--- Areas/Admin/Controllers/CategoryController.cs
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -39,6 +39,27 @@ namespace BulkyBook.Areas.Admin.Controllers
             return View(category);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.Id == 0)
+                {
+                    _unitOfWork.Category.Add(category);
+
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(category);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
         #region API CALLS
         [HttpGet]
         public IActionResult GetAll()
@@ -46,6 +67,9 @@ namespace BulkyBook.Areas.Admin.Controllers
             var allObj = _unitOfWork.Category.GetAll();
             return Json(new { data = allObj });
         }
+
+        [HttpDelete]
+        public IActionResult 
         #endregion
     }
 }
